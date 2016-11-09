99fef33e6acd80683301812d9661a535de53d4e5
James Ahlm <14782828428@qq.com>
2016-11-09 11:06:00 +0800

    Software displays correctly, language has an extra comma

---
+++ server.js
@@ -0,0 +1,17 @@
+var express = require('express')
+var app = express()
+
+app.get('/', function (req, res) {
+　　var language = req.get('Accept-Language');
+　　language = language.split(';');
+  language = language[0];
+　　var user_agent = req.get('User-Agent');
+　　var u_a = user_agent.split('(')[1];
+　　var software = u_a.split(')')[0];
+  var ip = req.ip;
+ 　res.send(JSON.stringify({ipaddress: ip, language: language, software: software}));
+})
+
+app.listen(3000, function () {
+  console.log('Example app listening on port 3000!')
+})
