a68f14cf29ba8635941ebd39dba37f13aa7a7580
James Ahlm <14782828428@qq.com>
2016-12-27 14:40:57 +0800

    Added Usage Page

--- server.js
+++ server.js
@@ -1,7 +1,12 @@
 var express = require('express')
 var app = express()
+app.use('/public', express.static(process.cwd() + '/public'));
 
-app.get('/', function (req, res) {
+app.get('/', function(req,res){
+  res.sendFile(process.cwd() + '/public/index.html');
+});
+
+app.get('/header', function (req, res) {
 　　var language = req.get('Accept-Language');
 　　language = language.split(';');
   language = language[0];
